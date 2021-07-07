6bbaff51b86541732d4c4cef758bfa563cb0f0cd
unknown <you@example.com>
2021-07-07 17:24:30 -0400

    07072021 5:24pm

--- src/hw3Q3JavaBasics/MyAddress.java
+++ src/hw3Q3JavaBasics/MyAddress.java
@@ -1,30 +1,24 @@
 package hw3Q3JavaBasics;
 
-
-
-
 public class MyAddress {
+//we want to change the allignment//
+	public static void main(String[] args) {
 
-	
-public static void main(String[]args) {
-	
-	String MyName = "Boby";
-	int houseNumber = 123;
-	char houseDiraction = 'W';
-	String CityName = "New York";
-	String State = "NY";
-	int zipCode = 12345;
-	boolean inUSA = true;
-				
-	System.out.println(MyName);
-	System.out.println(houseNumber);
-	System.out.println(houseDiraction);
-	System.out.println(CityName);
-	System.out.println(State);
-	System.out.println(inUSA);
-		
+		String MyName = "Boby";
+		int houseNumber = 123;
+		char houseDiraction = 'W';
+		String CityName = "New York";
+		String State = "NY";
+		int zipCode = 12345;
+		boolean inUSA = true;
 
-    }
+		System.out.println(MyName);
+		System.out.println(houseNumber);
+		System.out.println(houseDiraction);
+		System.out.println(CityName);
+		System.out.println(State);
+		System.out.println(inUSA);
 
+	}
 
 }
